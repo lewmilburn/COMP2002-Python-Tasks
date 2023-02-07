d7bb697a0fb0793a919b2becedc60d95ab40c5a7
lewmilburn <63267144+lewmilburn@users.noreply.github.com>
2023-02-07 15:30:21 +0000

    Updated 1-3

--- 1-3 Numby-Matplot.py
+++ 1-3 Numby-Matplot.py
@@ -1,10 +1,15 @@
 # ---------------------------------- #
 #     COMP20O2 PROGRAMMING TASKS     #
 #                                    #
-#  WEEK 1, TASK 2 - NUMBY & MATPLOT  #
+#  WEEK 1, TASK 3 - NUMBY & MATPLOT  #
 #                                    #
 #        STARTED 7TH FEB 2023        #
 #       COMPLETED 7TH FEB 2023       #
 # ---------------------------------- #
 
 import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.linspace(-np.pi, np.pi, 256, True)
+
+plt.plot(x, np.sin(x))
