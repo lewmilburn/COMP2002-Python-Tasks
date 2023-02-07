7c423df339664a8d67a4176e22485e219fa1ce94
lewmilburn <63267144+lewmilburn@users.noreply.github.com>
2023-02-07 15:24:04 +0000

    Completed bubble sort, started numby

--- 1-1 Bubble Sort.py
+++ 1-1 Bubble Sort.py
@@ -6,3 +6,16 @@
 #        STARTED 7TH FEB 2023        #
 #       COMPLETED 7TH FEB 2023       #
 # ---------------------------------- #
+
+def bubblesort(nlist):
+    for passnum in range(len(nlist) - 1, 0, -1):  # in range of the list
+        for i in range(passnum):  # if i is in the range that passum is in
+            if nlist[i] > nlist[i + 1]:  # if n list i is greater than n list i+1
+                temp = nlist[i]  # temp number to make sure previous one is saved
+                nlist[i] = nlist[i + 1]  # This will overide with the next number in the list
+                nlist[i + 1] = temp  # Stores the number you store in temp in list
+
+
+nlist = [14, 46, 43, 27, 57, 41, 45, 21, 70]
+bubblesort(nlist)
+print(nlist)
---
+++ 1-2 Numby-Matplot.py
@@ -0,0 +1,10 @@
+# ---------------------------------- #
+#     COMP20O2 PROGRAMMING TASKS     #
+#                                    #
+#  WEEK 1, TASK 2 - NUMBY & MATPLOT  #
+#                                    #
+#        STARTED 7TH FEB 2023        #
+#       COMPLETED 7TH FEB 2023       #
+# ---------------------------------- #
+
+import numpy as np
